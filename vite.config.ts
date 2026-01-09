8c7f8268a9c24afddbd2035b64324b6af551bc57
Dawn Connell <dawnthomsonconnell@gmail.com>
2026-01-09 12:05:56 +1100

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "experiments-in-vue",
+  base: "/experiments-in-vue/",
   plugins: [vue()],
 });
